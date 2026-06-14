bdb9c7cb9d75f1cb93cba84ebd037b2d4e6b59a2
litijun <913592585@qq.com>
2026-06-15 03:22:04 +0800

    Fix iOS project: add Debug.xcconfig, Release.xcconfig, AppFrameworkInfo.plist, storyboards, GeneratedPluginRegistrant

---
+++ ios/Runner/Runner-Bridging-Header.h
@@ -0,0 +1,11 @@
+//
+//  Runner-Bridging-Header.h
+//
+//  Bridging header for Runner target
+//
+
+#ifndef Runner_Bridging_Header_h
+#define Runner_Bridging_Header_h
+
+
+#endif /* Runner_Bridging_Header_h */
